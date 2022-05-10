5ca2f7f9f0bce0c9e6f109b48963c496e36a09a3
John Randall <john@johnrandall.com>
2022-05-10 12:31:54 -0400

    Cleanup: controller db querries

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,6 +1,8 @@
 class AnswersController < ApplicationController
   def index
-    @question = Question.find(params[:question_id])
+    @question = Question
+                  .eager_load(:answers)
+                  .find(params[:question_id])
     @answers= @question.answers
   end
 end
--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,11 +1,14 @@
 class SurveysController < ApplicationController
   def index
-    @surveys = Survey.all
-                     .eager_load(questions: :answers)
+    @surveys = Survey
+                 .eager_load(questions: :answers)
+                 .all
   end
 
   def show
-    @survey = Survey.eager_load(questions: :answers).find(params[:id])
+    @survey = Survey
+                .eager_load(questions: :answers)
+                .find(params[:id])
     @questions = @survey.questions
   end
 end
